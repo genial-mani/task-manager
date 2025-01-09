78d61bf97e9052a539cc5b40a31f35ffb69872c3
genial-mani <manikantaginige1@gmail.com>
2025-01-09 18:36:16 +0530

    initial commit

---
+++ src/app/components/providers/toastProvider.tsx
@@ -0,0 +1,5 @@
+import { Toaster } from "sonner";
+
+export default function ToastProvider({children}: Readonly<{children: React.ReactNode}>) {
+    return <div>{children}<Toaster richColors position="top-right"/></div>
+}
