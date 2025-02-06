2f6527fffa6edb6e3b846ee43fd99e616d0bf51c
genial-mani <manikantaginige1@gmail.com>
2025-02-07 00:44:43 +0530

    middleware.ts edited

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,12 @@ import type { NextRequest } from "next/server";
 import { jwtVerify } from "./utils/jwt";
 
 export async function middleware(req: NextRequest) {
-    const token = req.cookies.get("token")?.value;
-    if (!token) {
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
-
+    
     try {
+        const token = req.cookies.get("token")?.value;
+        if (!token) {
+            return NextResponse.redirect(new URL("/login", req.url));
+        }
         const payload = await jwtVerify(token);
         if (!payload) {
             return NextResponse.redirect(new URL("/login", req.url));
