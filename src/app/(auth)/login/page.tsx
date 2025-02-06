378a694c1b8d56eb082ac3d538276fb0593a5d7f
genial-mani <manikantaginige1@gmail.com>
2025-02-07 00:52:18 +0530

    redirection changed after login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -38,7 +38,7 @@ export default function Login() {
       const data = await response.json();
       if (response.ok) {
         toast.success(data.message);
-        router.replace("/");
+        router.replace("/tasks");
       }
       if (!response.ok) {
         toast.error(data.error);
