e772dcaec700df73b2aff7f8d652725f251db19d
genial-mani <manikantaginige1@gmail.com>
2025-04-05 14:29:12 +0530

    after login routing change

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,20 +1,14 @@
-import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import { HiOutlineLogout } from "react-icons/hi";
-import { useEffect, useState } from "react";
-import { userType } from "@/utils/Interfaces";
 
 export default function Logout() {
-    const router = useRouter();
-    const [user, setUser] = useState<userType | null>(null);
     const handleLogout = async ()=>{
         try {
             const response = await fetch('/api/auth/logout',{method: 'POST',cache: 'no-store'});
             const data = await response.json();
             if(response.ok){
                 localStorage.removeItem('user');
-                setUser(null);
-                router.push('/');
+                window.location.href = '/';
                 toast.success(data.message);
             }
             else{
@@ -27,11 +21,6 @@ export default function Logout() {
         }
     }
 
-    useEffect(()=>{
-        if(!user){
-          window.location.href = "/";
-        }
-      },[user])
 
     return <div onClick={handleLogout} className="flex items-center gap-1 py-2 px-3 rounded-lg bg-[#a855f7] text-seasalt cursor-pointer"> <HiOutlineLogout/> Logout</div>;
 }
