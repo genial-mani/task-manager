91e3a544ff2f57e270815cc5e3a07334b3f4bde4
genial-mani <manikantaginige1@gmail.com>
2025-06-01 23:31:34 +0530

    added login route to the middleare config

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -41,7 +41,7 @@ export default function Login() {
       const data = await response.json();
       
       if (response.ok) {
-        localStorage.setItem("user", data.user);
+        localStorage.setItem("user", JSON.stringify(data.user));
         toast.success(data.message);
         toast.dismiss();
         // toast.info("Redirecting to tasks page..."); this line is not needed because already redirecting
--- src/middleware.ts
+++ src/middleware.ts
@@ -33,6 +33,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/dashboard/:path*', '/tasks/:path*'],
+    matcher: ['/dashboard/:path*', '/tasks/:path*','/login'],
     runtime: 'nodejs',
 };
